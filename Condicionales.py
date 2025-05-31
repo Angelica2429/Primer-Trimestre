4ce415bae9ace867ab3a72c898dcd85ccd16e527
Angelica2429 <benavidescabezas.angelica@gmail.com>
2025-05-31 17:47:07 -0500

    Condicionales.py

---
+++ Condicionales.py
@@ -0,0 +1,29 @@
+edad=int(input("Ingresa tu edad:"))
+if edad >= 18:
+    print("Eres mayor de edad")
+
+num1=int(input("Ingrese el número que desea multiplicar"))
+num2=int(input("Ingrese el segundo número que desea multiplicar"))
+
+if num1 < 100:
+    print("El número aplica")
+elif num2 < 100:
+    print("El número aplica")
+else:
+    print("NADA")
+
+
+Edad=int(input("Ingrese su edad:"))
+if Edad <=1920:
+    print("Eres genaracion silenciosa")
+elif Edad <=1940:
+    print("Eres de la generación silenciosa")
+elif Edad == 1946:
+    print("Eres de la generación de los baby boomer")
+elif Edad <=1964:
+    print("Eres de la generación de los baby boomers")  
+elif Edad ==1964:
+    print("Eres de la generación X")
+elif Edad <=1979:
+    print("Eres de la generación x")
+    
