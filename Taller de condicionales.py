69f2c3ebd9f7b1b7851b5f86e027855e2343c500
Angélica2429 <benavidescabezas.angelica@gmail.com>
2025-06-07 17:33:10 -0500

    Condicionales.

--- Taller de condicionales.py
+++ Taller de condicionales.py
@@ -45,31 +45,86 @@
 # else:
 #     print("No se sabe")
 
-print("Ejercicio #6")
-produc1=int(input("Ingrese el precio del primer producto:"))
-produc2=int(input("Ingrese el precio del segundo número:"))
-precio_total=produc1+produc2
-des=0.10/precio_total
-if precio_total >= 100:
-    des=0.10/precio_total 
-    print(f"tiene un descuento del 10% su total a pagar es de {des}")
+# print("Ejercicio #6")
+# produc1=int(input("Ingrese el precio del primer producto:"))
+# produc2=int(input("Ingrese el precio del segundo número:"))
+# precio_total=produc1+produc2
+# porce=10                                             #establezco o pregunto el porcentaje,luego hago una operacion para sacar el dewscuento del precio final
+# if precio_total >= 100:                              #es poner el precio final luego eso lo divido por 100 que seria el porcentaje final y por ultimo ese resultado lo multiplico por el porcentaje del descuento
+#     des=precio_total/100*porce
+#     print(f"tiene un descuento del 10% su descuento es de {des}")
+# else:
+#     print("No tiene descuento")
+
+# print("Ejercicio #7")
+# edad=int(input("Ingresa tu edad:"))
+# if edad >= 18:
+# #     print("Puedes votar")
+# # else:
+# #     print("No puedes votar")
+
+# print("Ejercicio #8")
+# precio=int(input("Ingrese el precio del producto:"))
+# tipo_de_cliente=input("¿Eres VIP o normal?:").lower()
+# if tipo_de_cliente=="vip":
+#     porcentaje=precio/100 *20
+#     print(f"Su descuento como VIP será de {porcentaje}")
+# else:
+#     print("No tienes descuento ")
+
+# print("Ejercicio #9")
+# num10=int(input("Ingrese un número:"))
+# multi=(5*(num10//5))-(3*(num10//3)) 
+# if  multi==0:
+#     print("El número es multiplo del 5 y al 3")
+# else:
+# #     print("El número no es multiplo del 5 o 3")
+# print("Ejercicio #10")
+# num11=int(input("Ingrese un número:"))
+# num12=int(input("Ingrese un número:"))
+# multi2=(num12*(num11//2))-(num11*(num11//2))
+# if  multi2==0:
+#     print("El número es divisible entre dos")
+# else:
+#     print("El número no es divisible entre dos")
+
+# # print("Ejercicio #11")
+# lista1=[]
+# lista1=int(input("Ingrese un número:")),int(input("Ingrese un número:")),int(input("Ingrese un número:")),int(input("Ingrese un número:")),int(input("Ingrese un número:"))
+# print(lista1)
+# if lista1[2] < 10:
+#     print(f"{lista [2]} es menor a 10")
+# else:
+#     print(f"{lista [2]} es mayor a 10")
+
+# print("Ejercicio #12")
+# lista2=[3,5,7,9]
+# if lista2[2] == 7:
+#     print(f"la lista tiene {lista[2]}")
+# else:
+#     print("El número no tiene 7")
+
+# print("Ejercicio #13")
+# lista3=[4,6,2,8]
+# ope=lista3[0]+lista3[1]
+# if ope > 10:
+#     print("La suma es alta")
+# else:
+#     print("La suma es baja")
+
+# print("Ejercicio #14")
+# lista4=["Ana","Luis","Pedro","Marta"]
+# if lista4 [3]== "Marta":
+#     print("El  nombre es correcto")
+# else:
+#     print("El número no es correcto")
+
+print("Ejercicio #15")
+lista5=[]
+lista5=input("Ingrese un color:"),input("Ingrese un color:"),input("Ingrese un color:").lower()
+if lista5[1]=="azul":
+    n=lista5.([1])
+    a=lista5[1].append(input("Ingrese el nuevo color:"))
+    print(f"El nueva lista actualizada quedó así:{lista5}")
 else:
-    print("No tiene descuento")
-
-print("Ejercicio #7")
-edad=int(input("Ingresa tu edad:"))
-if edad >= 18:
-    print("Puedes votar")
-else:
-    print("No puedes votar")
-
-print("Ejercicio #8")
-precio=int(input("Ingrese el precio del producto:"))
-tipo_de_cliente=input("¿Eres VIP o normal").lower()
-if tipo_de_cliente== vip:
-    porcentaje=precio
-
-
-print("Ejercicio #9")
-num10=int(input("Ingrese un número:"))
-multi=  (5*(num1//3))
+    print("No es necesario cambiar el color")
