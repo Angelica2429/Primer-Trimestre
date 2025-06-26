3e9c66124d34291200822a9e8492c4a19d3bc97e
Angélica Benavides <benavidescabezas.angelica@gmail.com>
2025-06-26 17:48:29 -0500

    Create ejercicios_bucles

---
+++ ejercicios_bucles.py
@@ -0,0 +1,40 @@
+# print("Ejercicio # 1")
+# total=0
+# num1=int(input("Ingrese el primer número a sumar:"))
+# while total != 0:
+#      total += num1
+#      num1=int(input("Ingrese el primer número a sumar:"))
+#      print(f"Su total es {total}")
+   
+# print("Ejercicio # 2")
+# clave="python123"
+# intentos= 0
+# while clave !=:
+#     contra=input("Ingrese contraseña:")
+#     if contra == clave:
+#         print("Puede ingresar")
+#     intentos += 1
+# print("Ejercicio # 3")
+# productos=[]
+# agregar=(input("Ingrese un producto:"))
+# while agregar != "fin":
+#     productos.append(agregar)
+#     agregar=(input("Ingrese un producto:"))
+# print(productos)
+# print("Programa finalizado")
+
+print("Ejercicio # 4")
+numero=int(input("Ingrese el número con que quiere empezar a contar (del 1 al 10):"))
+while numero < 10:
+     numero += 1
+     if numero % 2 == 0:
+        print(f"{numero} es par")
+     else:
+        print(f"{numero} es impar")
+
+print("Ejercicio # 5")
+notas=[]
+agre1=(input("Ingrese un producto:"))
+while agre1 != "fin":
+    notas.append(agre1)
+    agre1=(input("Ingrese un producto:"))
